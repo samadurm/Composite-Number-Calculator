a39859d4a9a608a9b53dd329db76458d4e6313f1
samadurm <43584327+samadurm@users.noreply.github.com>
2019-02-17 17:58:16 -0800

    Got isComposite working

--- program4.asm
+++ program4.asm
@@ -17,7 +17,12 @@ instructions	 BYTE	   "Enter the number of composites to display between 1 and 4
 outOfRange	 BYTE	   "Number entered was out of range. Please try again: ",0
 goodbye		 BYTE	   "Goodbye!",0
 whitespace	 BYTE	   "   ",0
+divisor	  	 DWORD	   2
 index		 WORD	   0
+incorrectRange	 WORD	   0
+dividend		 DWORD	   ?
+compositeNum	 DWORD	   0
+
 .code
 main PROC
 
@@ -46,9 +51,12 @@ introduction ENDP
 ;=entered data is in the specified range					=   
 ;=================================================================
 getUserData PROC
+try_again:
 call	   ReadInt
 call	   CrLf
 call	   validate
+cmp	   incorrectRange, 1
+je	   try_again
 ret
 getUserData ENDP
 
@@ -57,24 +65,25 @@ getUserData ENDP
 ;=displays error message if not and reprompts for user input= 
 ;============================================================
 validate	  PROC
-push	   eax		    ;makes sure eax doesnt get changed by validation program
-cmp	   eax, 1
+mov	   incorrectRange, 0	  ;initially set the out of range to false
+push	   eax				  ;makes sure eax doesnt get changed by validation program
+cmp	   eax, LOWER_LIM
 jl	   range_error
-cmp	   eax, 400
+cmp	   eax, UPPER_LIM
 jg	   range_error
-jmp	   validate_end    ;else statement
+jmp	   validate_end		  ;else statement
 
 ;out of range segment
 range_error:
 mov	   edx, OFFSET outOfRange
 call	   WriteString
 call	   CrLf
-call	   getUserData	    ;see if this works
+mov	   incorrectRange, 1	  ;set incorrectRange to true
 jmp	   validate_end
 
 validate_end:
 pop	   eax		 ;restores eax
-ret
+ret	   ;try returning a value.
 validate	  ENDP
 
 ;=============================================================
@@ -85,11 +94,14 @@ showComposites	 PROC
 push	   eax
 push	   ecx
 mov	   ecx, eax
+mov	   eax, 4		;start at first composite number
 
 top_of_loop:
-call	   isComposite
+call	   isComposite		   ;check if current number is composite
+cmp	   compositeNum, 1
+jne	   not_composite
+
 call	   WriteDec
-inc	   eax
 inc	   index
 mov	   edx, OFFSET whitespace
 call	   WriteString
@@ -98,10 +110,13 @@ call	   WriteString
 cmp	   index, 10
 jne	   bottom_loop
 call	   CrLf
-mov	   index, 0
+mov	   index, 0		   ;reset index
 
-;else
+not_composite:
+inc	   ecx
+
 bottom_loop:
+inc	   eax
 loop	   top_of_loop
 
 call	   CrLf
@@ -113,7 +128,29 @@ showComposites	 ENDP
 ;===========================================
 ;=Checks if the current number is composite=
 ;===========================================
+;checking value in eax
 isComposite PROC
+push	   eax
+mov	   dividend, eax
+mov	   compositeNum, 0	   ;set composite number to false at start of loop
+mov	   divisor, 2		   ;start the divisor at 2
+
+while_loop:
+cdq
+div	   divisor
+mov	   eax, dividend	   ;restore eax to original value
+cmp	   edx, 0			   ;check if divisor divided evenly into dividend
+je	   out_of_loop
+inc	   divisor		   ;else increment divisor and try again
+cmp	   divisor, eax
+jl	   while_loop
+jmp	   else_descision
+
+out_of_loop:
+mov	   compositeNum, 1	   ;1 if composite
+	   
+else_descision:
+pop	   eax
 ret
 isComposite ENDP
 
